bf28f6b1fe842134364189f71c25aaedca829a6f
egcharchi <eric.charchi@student.moringaschool.com>
2023-02-20 11:34:22 +0300

    Final adjustments to JenkinsFile

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://morip01:UuvSN8c7T4yIFB59@g@moringaip1.mqq3hpt.mongodb.net/?retryWrites=true&w=majority',
-    development: 'mongodb+srv://morip01:UuvSN8c7T4yIFB59@@moringaip1.mqq3hpt.mongodb.net/?retryWrites=true&w=majority',
-    test: 'mongodb+srv://morip01:UuvSN8c7T4yIFB59@@moringaip1.mqq3hpt.mongodb.net/?retryWrites=true&w=majority',
+    production: 'mongodb+srv://morip01:QwOi07IsVywGlv0B@moringaip1.mqq3hpt.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://morip01:QwOi07IsVywGlv0B@moringaip1.mqq3hpt.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://morip01:QwOi07IsVywGlv0B@moringaip1.mqq3hpt.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
