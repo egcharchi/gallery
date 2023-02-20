9ce3d062d5087f816ef0ac7400b42605a0ec134c
egcharchi <eric.charchi@student.moringaschool.com>
2023-02-20 11:06:34 +0300

    Made adjustments to _Config

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://morip01:UuvSN8c7T4yIFB59@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://morip01:UuvSN8c7T4yIFB59@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://morip01:UuvSN8c7T4yIFB59@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://morip01:UuvSN8c7T4yIFB59@g@moringaip1.mqq3hpt.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://morip01:UuvSN8c7T4yIFB59@@moringaip1.mqq3hpt.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://morip01:UuvSN8c7T4yIFB59@@moringaip1.mqq3hpt.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
